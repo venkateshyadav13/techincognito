ec3df22467c23cba6763db15c3c601661b881695
Venkatesh Yadav <87112129+venkateshyadav13@users.noreply.github.com>
2024-05-20 23:30:32 +0530

    Update Shopfiy+Braintree10$.php

--- gates/Shopfiy+Braintree10$.php
+++ gates/Shopfiy+Braintree10$.php
@@ -61,13 +61,12 @@ if (in_array($userId, $owners)) {
     }
 }
 
-$gcm = "";
 //=======RANK DETERMINE END=========//
 $update = json_decode(file_get_contents("php://input"), TRUE);
 $text = $update["message"]["text"];
 //========WHO CAN CHECK FUNC========//
 $r = "0";
-
+$gcm = "sb";
 $r = rand(0, 100);
 //=====WHO CAN CHECK FUNC END======//
 if (preg_match('/^(\/sb|\.sb|!sb)/', $text)) {
