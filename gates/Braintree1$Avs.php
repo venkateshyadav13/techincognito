5307f2963a13de75f4601f0874842c3b51ddb1df
Venkatesh Yadav <87112129+venkateshyadav13@users.noreply.github.com>
2024-07-01 17:28:38 +0530

    Delete gates directory

--- gates/Braintree1$Avs.php
+++
@@ -1,9 +0,0 @@
-  <?php
-
-    if (preg_match('/^(\/ba|\.ba|!ba)/', $text)) {
-      $userid = $update['message']['from']['id'];
-
-        $sent_message_id = send_reply($chatId, $message_id, $keyboard, "<b>THIS GATE ON MAINTENANCE ❌</b>");
-
-
-  }
